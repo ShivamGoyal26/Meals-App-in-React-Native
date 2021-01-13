e017794cecda504d593c6c5acecd4a4e46dffc1d
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2021-01-13 12:22:23 +0530

    Swtiches has been added in Filters

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Switch } from 'react-native';
 import FilterHeader from '../shared/FilterHeader';
 import Colors from '../constants/Colors';
 
-
+const FilterSwitch = props => {
+    return (
+        <View style={styles.filterContainer}>
+            <Text style={styles.filterTitle}>{props.lable}</Text>
+            <Switch
+                trackColor={{ true: Colors.primaryColor, false: '#C0C0C0' }}
+                thumbColor={Colors.primaryColor}
+                value={props.state}
+                onValueChange={props.onChange}
+            />
+        </View>
+    );
+};
 
 const FiltersScreen = props => {
+    const [isGlutenFree, setIsGlutenFree] = useState(false);
+    const [isLactoseFree, setIsLactoseFree] = useState(false);
+    const [isVegan, setIsVegan] = useState(false);
+    const [isVegetarian, setIsVegetarian] = useState(false);
     return (
         <View style={styles.screen}>
             <FilterHeader titleText='Filters' onSelect={() => {
@@ -15,20 +31,27 @@ const FiltersScreen = props => {
             <View style={styles.filterScreen}>
                 <Text style={styles.title}>Available Filters</Text>
 
-                <View style={styles.filterContainer}>
-                    <Text style={styles.filterTitle}>Gluten-Free</Text>
-                    <Switch />
-                </View>
 
-                <View style={styles.filterContainer}>
-                    <Text style={styles.filterTitle}>Gluten-Free</Text>
-                    <Switch />
-                </View>
 
-                <View style={styles.filterContainer}>
-                    <Text style={styles.filterTitle}>Gluten-Free</Text>
-                    <Switch />
-                </View>
+                <FilterSwitch lable="Gluten-Free"
+                    state={isGlutenFree}
+                    onChange={newValue => setIsGlutenFree(newValue)}
+                />
+
+                <FilterSwitch lable="Lactose-Free"
+                    state={isLactoseFree}
+                    onChange={newValue => setIsLactoseFree(newValue)}
+                />
+
+                <FilterSwitch lable="Vegan"
+                    state={isVegan}
+                    onChange={newValue => setIsVegan(newValue)}
+                />
+
+                <FilterSwitch lable="Vegetarian"
+                    state={isVegetarian}
+                    onChange={newValue => setIsVegetarian(newValue)}
+                />
 
             </View>
 
